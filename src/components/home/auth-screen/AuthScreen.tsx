4f37115d2b234583ab9dbe916a8e157f9e94b86d
mutant1o1 <chauhan.sahil.2022@gmail.com>
2024-06-23 13:30:08 +0530

    Nextjs and ShadCN setup

---
+++ src/components/home/auth-screen/AuthScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "./HeroSection";
+
+const AuthScreen = () => {
+  return (
+    <div className="flex flex-col text-primary">
+      <HeroSection/>
+    </div>
+  );
+};
+
+export default AuthScreen;
